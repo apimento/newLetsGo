8182ac960b20e88fdcbfb68ed1ac86816f735527
apimento <98665437+apimento@users.noreply.github.com>
2022-03-15 15:09:44 -0400

    issue with colorchange fixed

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
 $('.alert').hide(3000); 
 
 const rating = document.getElementById("rating"); 
-if (rating.outerText >= "8"){ 
+if (rating.outerText >= 8 ){ 
     rating.style = "color: rgb(90, 238, 31)";
 } else if (rating.outerText <= 5) { 
     rating.style = "rgb(235,49,49)";
